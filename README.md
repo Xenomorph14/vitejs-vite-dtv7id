a8164c7b86b1b722b682a7da472f839f24f32e39
Xenomorph14 <62198279+Xenomorph14@users.noreply.github.com>
2023-04-15 15:26:18 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-dtv7id
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-dtv7id)
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue';
+import {} from 'video.js';
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <link
+    href="https://unpkg.com/video.js@7/dist/video-js.min.css"
+    rel="stylesheet"
+  />
+
+  <!-- City -->
+  <link
+    href="https://unpkg.com/@videojs/themes@1/dist/city/index.css"
+    rel="stylesheet"
+  />
+
+  <!-- Then, in the player -->
+  <video class="video-js vjs-theme-city" ...></video>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
